599f90414c9a875246f18130761c8b4ac4444c1e
Suf <sufkarmon2@gmail.com>
2022-06-21 00:29:59 +0300

    personal porfolio first publish with php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    $dbhost = "148.66.138.145";
+    $dbuser = "portfolioSh";
+    $dbpass = "portShUsr21!";
+    $dbname = "portfolioSh";
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+    if(mysqli_connect_errno()){
+        die("DB connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+    }
+?>
---
+++ index.php
@@ -0,0 +1,343 @@
+<?php
+  include "db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Suf Karmon</title>
+  <meta content="" name="Suf Karmon">
+  <meta content="" name="Suf Karmon">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <!-- <button type="button" class="mobile-nav-toggle d-xl-none"><i class="bi bi-list mobile-nav-toggle"></i></button> -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex flex-column justify-content-center">
+
+    <nav id="navbar" class="navbar nav-menu">
+      <ul>
+        <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+        <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+        <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+        <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+        <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center">
+    <div class="container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Suf Karmon</h1>
+      <p>I'm a <span class="typed" data-typed-items="Software Developer"></span></p>
+      <div class="social-links">
+        <a href="https://www.facebook.com/suf.karmon" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/sufkarmon/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="www.linkedin.com/in/suf-karmon-3512621b2" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://github.com/sufka861" class="github"><i class="bx bxl-github"></i></a>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>I believe the best results are achieved through multi – disciplinary collaboration. So I have dedicated the past 9 years doing so.</p><p>
+            Now looking to contribute as a software developer.</p><p>
+            Combination of a software developer's knowledge,  experience of a technological units alumni, and elite military fighter's mentality – all in one.
+            </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4">
+            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Software Developer</h3>
+            <p class="fst-italic">
+              Finished 2nd year of B.Sc Softwate Engineering
+            </p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>28</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+972-54-2777105</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Address</Address>:</strong> <span>Tel-Aviv, Israel</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Languages</Address>:</strong> <span>Hebrew- native. English - native</span></li>
+
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>B.Sc Softwate Engineering</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>E-mail:</strong> <span>sufkarmon2@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Work status:</strong> <span>Available for work</span></li>
+                </ul>
+              </div>
+            </div>
+            <p>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Resume</h2>
+          <a href="assets/img/Resume-Suf-Karmon.pdf">Download resume</a>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+            <h3 class="resume-title">Skills</h3>
+            <div class="resume-item pb-0">
+              <h4>Programing languages</h4>
+              <ul>
+                <li>Frontend: HTML, CSS, Java-script</li>
+                <li>Backend: PHP</li>
+                <li>MySQL, JSON</li>
+                <li>C, C++</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>B.Sc Software Engineering</h4>
+              <h5>2020 - today</h5>
+              <p><em>Shenkar</em></p>
+              <p>Major in Web & Cloud development</p>
+              <p>Current GPA: 87</p>
+            </div>
+            <div class="resume-item">
+              <h4>Product manager course</h4>
+              <h5>2020</h5>
+              <p><em>IDF school of Computer and Cyber Professions ("Basmach")</em></p>
+            </div>
+            <div class="resume-item">
+              <h4>B.A Political Science </h4>
+              <h5>2014 - 2015</h5>
+              <p><em>Haifa University</em></p>
+              <p>Final GPA: 90</p>
+            </div>
+            <div class="resume-item">
+              <h4>Mevoot Iron high school</h4>
+              <h5>2009 - 2011</h5>
+              <p>Major – computer science, English translation, and music</p>
+            </div>
+
+          </div>
+          <div class="col-lg-6">
+            <h3 class="resume-title">Professional Experience</h3>
+            <div class="resume-item">
+              <h4>Product manager - IDF</h4>
+              <h5>2019 - 2022</h5>
+              <p><em>C4I department (Command Control Communications and Intelligence)</em></p>
+              <ul>
+                <li>Lead the development of 3 'In house' command & control systems</li>
+                <li>Initiated an 'operative need' from prototype development to full operational use and expanding to every small vessel in the Navy</li>
+                <li>2.5 years experience in Agile software development methods</li>
+                <li>Initiated digital transformation of navy commando vessels to integrate independent systems under a local cloud and shared resources.</li>
+                <li>Responsible for the full product life-cycle from early requirements, architecture, budget, roadmap, integration, development and prioritization alongside the R&D team</li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Captain</h4>
+              <h5>2017 - 2019</h5>
+              <p><em>Commander of a navy patrol boat</em></p>
+              <ul>
+                <li>In charge of a crew of 14 soliders and the Captain of the ship</li>
+                <li>Responsible for combat missions, training programs, technical maintenance, and general well – being of the soldiers </li>
+              </ul>
+            </div>
+            <div class="resume-item">
+              <h4>Cadet </h4>
+              <h5>2013 - 2016</h5>
+              <p><em>Naval Academy Course ("Hovlim")</em></p>
+            </div>
+            
+            <h3 class="resume-title">Volunteering</h3>
+            <div class="resume-item pb-0">
+              <h4>AUY</h4>
+              <p>Instructor of youth groups in the 'AUY' leading groups of teenagers in leadership and social activism work</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Portfolio</h2>
+          <p>Some of my projects so far</p>
+          <div class="social-links">
+            <a href="https://github.com/sufka861" class="github"><i class="bx bxl-github"></i></a>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+        
+          <?php
+          for($id = 1; $id <=5; $id++)
+          {
+              $query = "SELECT * FROM portfolio_SufKarmon WHERE ID = $id;";
+              $result = mysqli_query($connection, $query);
+              if($result){
+                $row = mysqli_fetch_assoc($result);
+              }
+              else die ("DB query failed");
+
+              echo "<div class='col-lg-4 col-md-6 portfolio-item filter-web'>";
+                echo "<div class='portfolio-wrap'>";
+                  echo "<img src='" . $row['image'] . "' class='img-fluid' alt=''>";
+                  echo "<div class='portfolio-info'>";
+                    echo "<h4>" . $row['name'] ."</h4>";
+                    echo "<p>" . $row['description'] . "</p>";
+                      echo "<div class='portfolio-links'>";
+                      echo "<a href='" . $row['project_URL'] . "' title='Project'><i class='bx bx-plus'></i></a>";
+                      echo "<a href='" . $row['github_URL'] ."' title='Git-Hub'><i class='bx bx-link'></i></a>";
+                    echo "</div>";
+                  echo "</div>";
+                echo "</div>";
+              echo "</div>";
+          }
+          ?>
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+  
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Contact</h2>
+        </div>
+
+        <div class="row mt-1">
+
+          <div class="col-lg-4">
+            <div class="info">
+              <div class="address">
+                <i class="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>Tel-Aviv, Israel</p>
+              </div>
+
+              <div class="email">
+                <i class="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>sufkarmon2@gmail.com</p>
+              </div>
+
+              <div class="phone">
+                <i class="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+972-54-2777105</p>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>Suf Karmon</h3>
+      <p></p>
+      <div class="social-links">
+        <a href="https://www.facebook.com/suf.karmon" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/sufkarmon/" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/suf-karmon-3512621b2" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="https://github.com/sufka861" class="github"><i class="bx bxl-github"></i></a>
+      </div>
+      <div class="copyright">
+        <div>
+          &copy; Copyright <strong><span>Suf Karmon</span></strong>. All Rights Reserved
+        </div>
+
+        <div>
+          <a href="https://www.shenkar.ac.il/he/departments/engineering-software-department">תואר ראשון בהנדסת תוכנה בשנקר</a>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
+
+<?php
+  mysqli_close($connection);
+?>
